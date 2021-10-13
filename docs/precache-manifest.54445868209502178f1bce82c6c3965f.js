87e8b3b274bfd95c0ed3f0dc02a5f7f47ee4359b
xxrl <837951112@qq.com>
2021-10-13 09:42:40 +0800

    add build in production

---
+++ docs/precache-manifest.54445868209502178f1bce82c6c3965f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d3210bcdb25e2acad09eaa06f589eb1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bd9a4376d4a6c1720148",
+    "url": "./static/css/2.f7861fd5.chunk.css"
+  },
+  {
+    "revision": "fa646391ee2a0a7ff895",
+    "url": "./static/css/main.754ff65a.chunk.css"
+  },
+  {
+    "revision": "bd9a4376d4a6c1720148",
+    "url": "./static/js/2.ae9910c6.chunk.js"
+  },
+  {
+    "revision": "522e03d803bfdb0e0cda4e24f58dcf37",
+    "url": "./static/js/2.ae9910c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51f2e41b3b56283fe3f4",
+    "url": "./static/js/3.62dbe472.chunk.js"
+  },
+  {
+    "revision": "fa646391ee2a0a7ff895",
+    "url": "./static/js/main.5503b8e9.chunk.js"
+  },
+  {
+    "revision": "dc097eb29cefd522e2c2",
+    "url": "./static/js/runtime-main.270ff1ff.js"
+  }
+]);
